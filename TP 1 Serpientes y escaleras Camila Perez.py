6af266e2596298879249944f9e3a53f90739bceb
caylen0 <perez9camila@gmail.com>
2021-11-17 19:35:14 -0300

    Update TP 1 Serpientes y escaleras Camila Perez.py

--- TP 1 Serpientes y escaleras Camila Perez.py
+++ TP 1 Serpientes y escaleras Camila Perez.py
@@ -2,6 +2,7 @@ import random
 
 #LO QUE HACE EL JUGADOR
 
+
 def casillero_cascara_de_banana(posicion_jugador: int) -> int:  # le resta 20 a la posicion del jugador
 	posicion_jugador = posicion_jugador - 20
 	return posicion_jugador
